3d4b696d14b622bd61103a20a6150f869b435030
lyraormktm96 <93657236+lyraormktm96@users.noreply.github.com>
2021-11-03 09:08:37 -0400

    initial commit

---
+++ Asterisk_Pyramid.py
@@ -0,0 +1,20 @@
+asterisks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for x in range(9):
+    if x == 1:
+        print("*")
+    elif x == 2:
+        print("**")
+    elif x == 3:
+        print("***")
+    elif x == 4:
+        print("****")
+    elif x == 5:
+        print("*****")
+    elif x == 6:
+        print("****")
+    elif x == 7:
+        print("***")
+    elif x == 8:
+        print("**")
+    elif x == 9:
+        print("*")
